e66376fcbf3643f277f54ba98fb38840470c1ffe
OttoMP <otto28mai@gmail.com>
2019-04-28 11:30:48 -0300

    ECM initializing (?)

--- PS_agent.py
+++ PS_agent.py
@@ -20,27 +20,51 @@ class Agent(object):
 class ECM():
 
     def __init__(self, actions, percepts):
-        ECM = Graph()
-        p_clips = []
-        a_clips = []
+        self.ECM = Graph()
+        self.p_clips = []
+        self.a_clips = []
+        # TODO revise action and percept types
+        action = self.ECM.new_vertex_property("string")
+        self.ECM.vertex_properties["action"] = action
+        percept = self.ECM.new_vertex_property("double")
+        self.ECM.vertex_properties["percept"] = percept
 
        ### Creating percepts-clips
         for p in percepts:
-            percept_clip = ECM.add_vertex()
-            p_clips.append(percept_clip)
+            percept_clip = self.ECM.add_vertex()
+            percept[percept_clip] = p
+            self.p_clips.append(percept_clip)
 
         ### Creating action-clips
         for a in actions:
-            action_clip = ECM.add_vertex()
-            a_clips.append(action_clip)
+            action_clip = self.ECM.add_vertex()
+            action[action_clip] = a
+            self.a_clips.append(action_clip)
 
         ### Creating edges between percepts-clips and action-clips
         for p in p_clips:
             for a in a_clips:
-                ECM.add_edge(p,a)
+                self.ECM.add_edge(p,a)
+
+        ### Initializing h-value
+        h_value = self.ECM.new_edge_property("double")
+        edges = self.ECM.get_edges()
+        for e in edges:
+            h_value[e] = 1
+        self.ECM.edge_properties["h_value"] = h_value
 
     def random_walk(self, percept):
-        pass
+        for v in self.ECM.vertices():
+            if self.ECM.vp.percept[v] == percept:
+                hopping_clip = v
+                break
+
+        while self.ECM.vp.action[hopping_clip] == "":
+            out_edges = hopping_clip.out_edges()
+
+            total_value = 0.0
+            for e in out_edges:
+                total_value += self.ECM.ep.h_value[e]
 
     def update(self):
         pass
