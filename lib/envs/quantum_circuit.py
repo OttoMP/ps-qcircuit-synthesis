cf9bd5149869dafd2c1bf3b4b73abb95165b7798
OttoMP <otto28mai@gmail.com>
2019-05-11 06:08:02 -0300

    2qubit environment working, need to check bell states value

--- lib/envs/quantum_circuit.py
+++ lib/envs/quantum_circuit.py
@@ -24,10 +24,11 @@ class ActionSpace(object):
 class QuantumCircuitEnv2Qubits(Environment):
 
     def __init__(self, max_circuit_depth, goal_state):
-        #super(QuatumCircuitEnv2Qubits, self).__init__()
+        super(QuantumCircuitEnv2Qubits, self).__init__()
         # define state and action space
         #self.S
-        self.action_space = ActionSpace(['Z', 'H'])
+        self.action_space = ActionSpace(['H1', 'CNOT10'])
+        #self.action_space = ActionSpace(['X0', 'Y0', 'Z0', 'H0', 'X1', 'Y1', 'Z1', 'H1', 'CNOT10'])
 
         # define reward structure
         #self.trace_distance()
@@ -37,7 +38,7 @@ class QuantumCircuitEnv2Qubits(Environment):
 
         self.max_circuit_depth = max_circuit_depth
         self.goal_state = goal_state
-        self.num_qubits = 1
+        self.num_qubits = 2
         self.tolerance = 0.001
 
         self.circuit_depths = np.zeros(self.num_qubits)
@@ -53,18 +54,39 @@ class QuantumCircuitEnv2Qubits(Environment):
 
     def action2matrix(self, action):
         return {
-                'X':np.matrix([[0,1],[1,0]]),
-                'Y':np.matrix([[0,-1j],[1j,0]]),
-                'Z':np.matrix([[1,0],[0,-1]]),
-                'H':1/np.sqrt(2) * np.matrix([[1,1],[1,-1]]),
+                'X0':np.matrix([[0,0,1,0],[0,0,0,1], [1,0,0,0], [0,1,0,0]]),
+                'Y0':np.matrix([[0,0,-1j,0],[0,0,0,-1j], [1j,0,0,0], [0,1j,0,0]]),
+                'Z0':np.matrix([[1,0,0,0],[0,1,0,0], [0,0,-1,0], [0,0,0,-1]]),
+                'H0':1/np.sqrt(2) * np.matrix([[1,0,1,0],[0,1,0,1], [1,0,-1,0], [0,1,0,-1]]),
+                'X1':np.matrix([[0,1,0,0],[1,0,0,0], [0,0,0,1], [0,0,1,0]]),
+                'Y1':np.matrix([[0,-1j,0,0],[-1j,0,0,0], [0,0,0,1j], [0,0,1j,0]]),
+                'Z1':np.matrix([[1,0,0,0],[0,-1,0,0], [0,0,1,0], [0,0,0,-1]]),
+                'H1':1/np.sqrt(2) * np.matrix([[1,1,0,0],[1,-1,0,0], [0,0,1,1], [0,0,1,-1]]),
+                'CNOT10':np.matrix([[1,0,0,0],[0,0,0,1], [0,0,1,0], [0,1,0,0]]),
                 }[action]
 
     def calculate_circuit_depth(self, a):
-        if a == 'X' or a == 'Y' or a == 'Z' or a == 'H':
+        if a == 'X0' or a == 'Y0' or a == 'Z0' or a == 'H0':
             self.circuit_depths[0] += 1
             if max(self.circuit_depths) > self.max_circuit_depth:
                 return
             else:
+                self.circuit_gates[0].append(a)
+        elif a == 'X1' or a == 'Y1' or a == 'Z1' or a == 'H1':
+            self.circuit_depths[1] += 1
+            if max(self.circuit_depths) > self.max_circuit_depth:
+                return
+            else:
+                self.circuit_gates[1].append(a)
+        elif a == 'CNOT10':
+            self.circuit_depths[1] = max(self.circuit_depths[0], self.circuit_depths[1])
+            self.circuit_depths[0] = max(self.circuit_depths[0], self.circuit_depths[1])
+            self.circuit_depths[1] += 1
+            self.circuit_depths[0] += 1
+            if max(self.circuit_depths) > self.max_circuit_depth:
+                return
+            else:
+                self.circuit_gates[1].append(a)
                 self.circuit_gates[0].append(a)
 
     def trace_distance(self, s):
@@ -79,7 +101,7 @@ class QuantumCircuitEnv2Qubits(Environment):
 
     def density_matrix(self,s):
         new_s = deepcopy(s)
-        new_s.shape = (2,1)
+        new_s.shape = (4,1)
         return new_s*np.conj(new_s).T
 
     def operate(self, s, a):
@@ -94,13 +116,18 @@ class QuantumCircuitEnv2Qubits(Environment):
         self.is_reset = False
 
         if (reward < -1. * (self.tolerance) or reward > self.tolerance) or max(self.circuit_depths) > self.max_circuit_depth:
-            print("Gates: ", self.circuit_gates)
+            output = open('output.out', 'a')
+            print("Gates:\n", file = output)
+            print("qubit 0: ", self.circuit_gates[0], file = output)
+            print("qubit 1: ", self.circuit_gates[1], file = output)
+            output.close()
+
             self.reset()
 
         return (self.s, reward, self.is_reset, '')
 
     def init_comp_basis(self):
-        basis = np.array([1,0])
+        basis = np.array([1,0,0,0])
         return basis
         '''
         basis = np.array([1,0])
@@ -109,3 +136,4 @@ class QuantumCircuitEnv2Qubits(Environment):
         tensor_basis.shape = (4,1)
         return tensor_basis.transpose()
         '''
+
--- run.py
+++ run.py
@@ -10,9 +10,11 @@ from lib.envs.quantum_circuit import QuantumCircuitEnv2Qubits
 from lib.simulation import Experiment
 
 interactive = False
-goal_state = np.array([0,1])
+zero = np.array([1,0,0,0])
+one = np.array([0,0,0,1])
+goal_state = 1/np.sqrt(2) * zero+one
 #env = SimpleRoomsEnv()
-env = QuantumCircuitEnv2Qubits(3,goal_state)
+env = QuantumCircuitEnv2Qubits(4,goal_state)
 agent = PS_agent(env.action_space.actions, [env.reset()], eta=0.1, gamma=0.001)
 experiment = Experiment(env, agent)
-experiment.run_ps(10, interactive)
+experiment.run_ps(200, interactive)
