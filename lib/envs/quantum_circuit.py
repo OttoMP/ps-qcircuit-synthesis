00e9249a310763bfe093acd0cf789fe3675bb237
OttoMP <otto28mai@gmail.com>
2019-05-13 09:59:22 -0300

    Shallow circuits receive more reward

--- lib/envs/quantum_circuit.py
+++ lib/envs/quantum_circuit.py
@@ -36,57 +36,64 @@ class QuantumCircuitEnv2Qubits(Environment):
         #self.operate()
 
         self.max_circuit_depth = max_circuit_depth
+        self.min_circuit_depth = max_circuit_depth
         self.goal_state = goal_state
         self.num_qubits = 2
         self.tolerance = 0.001
+        self.gate_matrices = {
+                'X0':(np.matrix([[0,0,1,0],[0,0,0,1], [1,0,0,0], [0,1,0,0]]), 0.77),
+                'Y0':(np.matrix([[0,0,-1j,0],[0,0,0,-1j], [1j,0,0,0], [0,1j,0,0]]), 0.77),
+                'Z0':(np.matrix([[1,0,0,0],[0,1,0,0], [0,0,-1,0], [0,0,0,-1]]), 0.77),
+                'H0':(1/np.sqrt(2) * np.matrix([[1,0,1,0],[0,1,0,1], [1,0,-1,0], [0,1,0,-1]]), 0.77),
+                'X1':(np.matrix([[0,1,0,0],[1,0,0,0], [0,0,0,1], [0,0,1,0]]), 1.46),
+                'Y1':(np.matrix([[0,-1j,0,0],[-1j,0,0,0], [0,0,0,1j], [0,0,1j,0]]), 1.46),
+                'Z1':(np.matrix([[1,0,0,0],[0,-1,0,0], [0,0,1,0], [0,0,0,-1]]), 1.46),
+                'H1':(1/np.sqrt(2) * np.matrix([[1,1,0,0],[1,-1,0,0], [0,0,1,1], [0,0,1,-1]]), 1.46),
+                'CNOT10':(np.matrix([[1,0,0,0],[0,0,0,1], [0,0,1,0], [0,1,0,0]]), 2.7),
+                }
 
         self.circuit_depths = np.zeros(self.num_qubits)
         self.circuit_gates = [[] for q in range(self.num_qubits)]
+        self.sum_error = 0
 
     def reset(self):
         self.s = self.init_comp_basis()
         self.circuit_depths = np.zeros(self.num_qubits)
         self.circuit_gates = [[] for q in range(self.num_qubits)]
+        self.sum_error = 0
         self.is_reset = True
 
         return self.s
 
     def action2matrix(self, action):
-        return {
-                'X0':np.matrix([[0,0,1,0],[0,0,0,1], [1,0,0,0], [0,1,0,0]]),
-                'Y0':np.matrix([[0,0,-1j,0],[0,0,0,-1j], [1j,0,0,0], [0,1j,0,0]]),
-                'Z0':np.matrix([[1,0,0,0],[0,1,0,0], [0,0,-1,0], [0,0,0,-1]]),
-                'H0':1/np.sqrt(2) * np.matrix([[1,0,1,0],[0,1,0,1], [1,0,-1,0], [0,1,0,-1]]),
-                'X1':np.matrix([[0,1,0,0],[1,0,0,0], [0,0,0,1], [0,0,1,0]]),
-                'Y1':np.matrix([[0,-1j,0,0],[-1j,0,0,0], [0,0,0,1j], [0,0,1j,0]]),
-                'Z1':np.matrix([[1,0,0,0],[0,-1,0,0], [0,0,1,0], [0,0,0,-1]]),
-                'H1':1/np.sqrt(2) * np.matrix([[1,1,0,0],[1,-1,0,0], [0,0,1,1], [0,0,1,-1]]),
-                'CNOT10':np.matrix([[1,0,0,0],[0,0,0,1], [0,0,1,0], [0,1,0,0]]),
-                }[action]
+        return self.gate_matrices[action]
 
     def calculate_circuit_depth(self, a):
         if a == 'X0' or a == 'Y0' or a == 'Z0' or a == 'H0':
             self.circuit_depths[0] += 1
             if max(self.circuit_depths) > self.max_circuit_depth:
-                return
+                return self.max_circuit_depth
             else:
                 self.circuit_gates[0].append(a)
+                return max(self.circuit_depths)
         elif a == 'X1' or a == 'Y1' or a == 'Z1' or a == 'H1':
             self.circuit_depths[1] += 1
             if max(self.circuit_depths) > self.max_circuit_depth:
-                return
+                return self.max_circuit_depth
             else:
                 self.circuit_gates[1].append(a)
+                return max(self.circuit_depths)
         elif a == 'CNOT10':
             self.circuit_depths[1] = max(self.circuit_depths[0], self.circuit_depths[1])
             self.circuit_depths[0] = max(self.circuit_depths[0], self.circuit_depths[1])
             self.circuit_depths[1] += 1
             self.circuit_depths[0] += 1
             if max(self.circuit_depths) > self.max_circuit_depth:
-                return
+                return self.max_circuit_depth
             else:
                 self.circuit_gates[1].append(a)
                 self.circuit_gates[0].append(a)
+                return max(self.circuit_depths)
 
     def trace_distance(self, s):
         density_s = self.density_matrix(s)
@@ -108,13 +115,17 @@ class QuantumCircuitEnv2Qubits(Environment):
 
     def step(self,action):
         s_prev = self.s
-        a = self.action2matrix(action)
+        a, e = self.action2matrix(action)
+        self.sum_error += e
         self.s = self.operate(self.s, a)
         reward = self.trace_distance(self.s)
-        self.calculate_circuit_depth(action)
+        depth = self.calculate_circuit_depth(action)
+        reward = (reward-self.sum_error) * (self.min_circuit_depth/depth)
+        if self.min_circuit_depth > depth:
+            self.min_circuit_depth = depth
         self.is_reset = False
 
-        if (reward < -1. * (self.tolerance) or reward > self.tolerance) or max(self.circuit_depths) > self.max_circuit_depth:
+        if reward > self.tolerance or max(self.circuit_depths) > self.max_circuit_depth:
             output = open('output.out', 'a')
             print("Gates:\n", file = output)
             print("qubit 0: ", self.circuit_gates[0], file = output)
