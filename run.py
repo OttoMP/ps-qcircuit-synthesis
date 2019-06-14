d774e442dfb214f20e45f814dbfd8b5bcbc451ef
OttoMP <otto28mai@gmail.com>
2019-06-14 12:02:22 -0300

    5-qubits circuit added

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from qutip import tensor, basis
 import numpy as np
 import sys
 
@@ -5,22 +6,38 @@ if "../" not in sys.path:
     sys.path.append("../")
 
 from PS_agent import PS_agent
-from envs.quantum_circuit import QuantumCircuitEnv2Qubits
+from envs.quantum_circuit import QuantumCircuitEnv2Qubits, QuantumCircuitEnv3Qubits, QuantumCircuitEnv4Qubits, QuantumCircuitEnv5Qubits
 from lib.simulation import Simulation
 
 interactive = False
 
 # Computational basis |00>, |01>, |10>, |11>
-zero  = np.array([1,0,0,0])
-one   = np.array([0,0,1,0])
-two   = np.array([0,1,0,0])
-three = np.array([0,0,0,1])
+zero  = tensor(basis(2,0), basis(2,0))
+one   = tensor(basis(2,0), basis(2,1))
+two   = tensor(basis(2,1), basis(2,0))
+three = tensor(basis(2,1), basis(2,1))
+
+b30 = tensor(basis(2,0), basis(2,0), basis(2,0))
+b31 = tensor(basis(2,1), basis(2,1), basis(2,1))
+
+b40 = tensor(basis(2,0), basis(2,0), basis(2,0), basis(2,0))
+b41 = tensor(basis(2,1), basis(2,1), basis(2,1), basis(2,1))
+
+b50 = tensor(basis(2,0), basis(2,0), basis(2,0), basis(2,0), basis(2,0))
+b51 = tensor(basis(2,1), basis(2,1), basis(2,1), basis(2,1), basis(2,1))
+
 
 # Bell State to be reached
-goal_state = 1/np.sqrt(2) * (zero+three)
+#goal_state = 1/np.sqrt(2) * (zero+three)
+#goal_state = 1/np.sqrt(2) * (b30+b31)
+#goal_state = 1/np.sqrt(2) * (b40+b41)
+goal_state = 1/np.sqrt(2) * (b50+b51)
 
 # Enviroments instantiation
-env = QuantumCircuitEnv2Qubits(4,goal_state, 1e-13)
+#env = QuantumCircuitEnv2Qubits(4, goal_state, 1e-13)
+#env = QuantumCircuitEnv3Qubits(5, goal_state, 1e-13)
+#env = QuantumCircuitEnv4Qubits(7, goal_state, 1e-13)
+env = QuantumCircuitEnv5Qubits(9, goal_state, 1e-13)
 
 # Agents instantiation
 agent = PS_agent(env.action_space, [env.reset()], eta=0.1, gamma=0.1)
@@ -29,4 +46,4 @@ agent = PS_agent(env.action_space, [env.reset()], eta=0.1, gamma=0.1)
 experiment = Simulation(env, agent)
 
 # Run simulation 200 times
-experiment.run_ps(150)
+experiment.run_ps(200)
